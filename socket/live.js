bf515406b2df206830b5f87e33eadf797b8d3507
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-12-04 12:59:50 +0100

    feat: server primus functionality done

--- socket/live.js
+++ socket/live.js
@@ -5,5 +5,18 @@ module.exports.go = (server) => {
     //check if primus connection ok, then console.log
     primus.on('connection', (spark) => {
         console.log('connected 🐥');
+
+        //check if data received from client, then console.log
+        spark.on('data', (data) => {
+            console.log("data received from client: 🙋🏼", data );
+            //if data action is 'scoreUpdate' then emit to all clients
+            if(data.action === 'scoreUpdate'){
+                primus.write({
+                    action: 'scoreUpdate', 
+                    team: data.team,
+                    score: data.score
+                });
+            }
+        });
     });
 }
